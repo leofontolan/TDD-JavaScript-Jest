a51ec7b56627d1a875f3cf6eaac107fb8442f341
leofontolan <leofontolan@gmail.com>
2022-04-01 13:40:41 -0300

    Organizando testes da Calculadora

--- jest-101/src/calc.test.js
+++ jest-101/src/calc.test.js
@@ -1,7 +1,13 @@
 var add = require('./calc')
 
-describe('calculator', function() {
-    it('add two numbers', function() {
-        expect(add(1, 2)).toBe(3)
-    })
-})
+describe('caculator', () => {
+    describe('should perform addition', () => {
+        it('add two positive numbers', () => {});
+        it('add two negative numbers', () => {});
+        it('adds a positive and a negative numbers', () => {});
+    });
+    
+    it('should perform subtraction', () => {});
+    it('should perform multiplication', () => {});
+    it('should perform division', () => {});
+});
